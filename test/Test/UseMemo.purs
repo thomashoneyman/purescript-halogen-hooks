440b23660f1d27053b077301ba1a4d482344d2d8
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-04-19 18:23:46 -0700

    Update UseMemo test to be more complex

--- test/Test/UseMemo.purs
+++ test/Test/UseMemo.purs
@@ -20,29 +20,47 @@ component :: forall q o m. H.Component HH.HTML q Unit o m
 component = Hooks.component \_ -> hook
 
 hook :: forall slots output m hooks
-      . Hooked slots output m hooks (Hooks.UseMemo Int (Hooks.UseState Int (Hooks.UseState Int hooks))) _
+      . Hooked slots output m hooks (Hooks.UseMemo Int (Hooks.UseMemo Int ((Hooks.UseMemo Int (Hooks.UseState Int (Hooks.UseState Int hooks)))))) _
 hook = Hooks.do
   s1 /\ ts1 <- Hooks.useState 0
   s2 /\ ts2 <- Hooks.useState 0
 
-  expensiveValue <- Hooks.captures { s1 } $ flip Hooks.useMemo \_ ->
-    let _ = unsafePerformEffect (Console.log "useMemo: recalculating expensive value")
-    in s1 + 5
+  expensiveValue1 <- memoize1 { s1 }
+
+  expensiveValue2 <- memoize2 { s2 }
+
+  expensiveValue3 <- memoize3 { s1, s2 }
 
   Hooks.pure $
     HH.div_
-      [ HH.p_ [ HH.text $ "Expensive value is: " <> show expensiveValue ]
+      [ HH.ol_
+        [ HH.li_ [ HH.text $ "Expensive value 1 is: " <> show expensiveValue1 ]
+        , HH.li_ [ HH.text $ "Expensive value 2 is: " <> show expensiveValue2 ]
+        , HH.li_ [ HH.text $ "Expensive value 3 is: " <> show expensiveValue3 ]
+        ]
       , HH.button
         [ HE.onClick \_ -> Just $ Hooks.modify_ ts1 (_ + 1)
         ]
         [ HH.text $
-            "Increase `s1` by 1, which will recompute the expensive value."
+            "Increase `s1` by 1, which will recompute 1 and 3."
         ]
       , HH.br_
       , HH.button
         [ HE.onClick \_ -> Just $ Hooks.modify_ ts2 (_ + 1)
         ]
         [ HH.text $
-            "Increase `s2` by 1, which will NOT recompute the expensive value."
+            "Increase `s2` by 1, which will recompute 2 and 3."
         ]
       ]
+  where
+    memoize1 deps@{s1} = Hooks.captures deps $ flip Hooks.useMemo \_ ->
+      let _ = unsafePerformEffect (Console.log "useMemo: recalculating expensive value1")
+      in s1 + 5
+
+    memoize2 deps@{s2} = Hooks.captures deps $ flip Hooks.useMemo \_ ->
+      let _ = unsafePerformEffect (Console.log "useMemo: recalculating expensive value2")
+      in s2 + 5
+
+    memoize3 deps@{s1, s2} = Hooks.captures deps $ flip Hooks.useMemo \_ ->
+      let _ = unsafePerformEffect (Console.log "useMemo: recalculating expensive value3")
+      in s1 + s2 + 5
