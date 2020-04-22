cd25ac7784f4d6224c33896b00aceb3c245c6ac6
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-04-22 08:00:16 -0700

    Inline useConditional failing example

--- test/Test/Manual/Conditional.purs
+++ test/Test/Manual/Conditional.purs
@@ -25,49 +25,33 @@ main = runHalogenAff do
   body <- awaitBody
   runUI component unit body
 
-newtype UseConditional hooks = UseConditional (UseEffect (UseState (Boolean) hooks))
-derive instance newtypeUseConditional :: Newtype (UseConditional hooks) _
-
-useConditional :: forall slots output m . MonadAff m => Boolean -> Hook slots output m UseConditional (Boolean)
-useConditional opt = Hooks.wrap Hooks.do
-  -- unique id to understand which hook execution is called by useEffect
-  id <- Hooks.pure $ show $ unsafePerformEffect $ randomInt 0 10000
-  Hooks.pure $ unsafePerformEffect $ liftEffect $ log $ "runConditional(" <> id <> "): " <> (show opt)
-  state /\ stateToken <- Hooks.useState false
-  useMyEffect stateToken id state { opt }
-  Hooks.pure state
-  where
-    useMyEffect stateToken id state deps@{ opt : opt' } = Hooks.captures deps Hooks.useTickEffect do
-      liftEffect $ log $ "CHANGE HANDLER TRIGGERED(" <> id <> "): " <> (show deps) <> ": " <> (show state) <> ":" <> (show opt')
-      Hooks.put stateToken opt'
-      pure Nothing
-
-newtype UseConditionalTest hooks = UseConditionalTest (UseConditional (UseEffect (UseState (Boolean) hooks)))
-derive instance newtypeUseConditionalTest :: Newtype (UseConditionalTest hooks) _
-
-useConditionalTest :: forall slots output m . MonadAff m => Hook slots output m UseConditionalTest (Boolean)
-useConditionalTest = Hooks.wrap Hooks.do
-  state /\ stateToken <- Hooks.useState false
-  useLifecycleEffect do
-    Hooks.put stateToken true
-    pure Nothing
-  val <- useConditional state
-  Hooks.pure val
-
 component :: forall query input output m. MonadAff m => Component HH.HTML query input output m
 component = Hooks.component $ \_ -> Hooks.do
   count /\ countToken <- Hooks.useState 0
-  val <- useConditionalTest
+  state1 /\ stateToken1 <- Hooks.useState false
+  useLifecycleEffect do
+    Hooks.put stateToken1 true
+    pure Nothing
+  -- unique id to understand which hook execution is called by useEffect
+  id <- Hooks.pure $ show $ unsafePerformEffect $ randomInt 0 10000
+  Hooks.pure $ unsafePerformEffect $ liftEffect $ log $ "runConditional(" <> id <> "): " <> (show state1)
+  state2 /\ stateToken2 <- Hooks.useState false
+  useMyEffect stateToken2 id state2 { state1 }
   Hooks.pure do
     HH.div_
       [
         HH.text $ "count: " <> (show count)
       , HH.br_
-      , HH.text $ "val: " <> (show val)
+      , HH.text $ "val: " <> (show state2)
       , HH.br_
       , HH.button [HE.onClick (\_ -> handleClick countToken count)] [HH.text "trigger count change" ]
       ]
 
   where
+    useMyEffect stateToken2 id state2 deps@{ state1 : state1' } = Hooks.captures deps Hooks.useTickEffect do
+      liftEffect $ log $ "CHANGE HANDLER TRIGGERED(" <> id <> "): " <> (show deps) <> ": " <> (show state2) <> ":" <> (show state1')
+      Hooks.put stateToken2 state1'
+      pure Nothing
+
     handleClick countToken count = Just do
       Hooks.put countToken (count + 1)
