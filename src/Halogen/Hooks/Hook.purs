6e4213772f255976f5feb47dec6b3e65adb6530c
Thomas R. Honeyman <thoneyman@awakesecurity.com>
2020-04-23 13:46:38 -0700

    Shift hook type definition up in the module

--- src/Halogen/Hooks/Hook.purs
+++ src/Halogen/Hooks/Hook.purs
@@ -14,6 +14,15 @@ import Halogen.Hooks.HookM (HookM, StateToken)
 import Halogen.Hooks.Internal.UseHookF (UseHookF(..))
 import Prelude (class Functor, Unit)
 
+-- | A function which has access to primitive and custom hooks like UseState,
+-- | UseEffect, UseRef, and UseMemo. Hook functions can be used to implement
+-- | reusable, stateful logic and to implement Halogen components.
+-- |
+-- | Functions of this type should be constructed using the Hooks API exposed
+-- | by `Halogen.Hooks`.
+type Hook ps o m (newHook :: Type -> Type) a
+  = forall hooks. Hooked ps o m hooks (newHook hooks) a
+
 -- | A largely internal type which underlies the `Hook` type. Used when the first
 -- | type variable of the indexed monad, `hooks`, cannot be hidden.
 newtype Hooked ps o m pre post a = Hooked (Indexed (Free (UseHookF ps o m)) pre post a)
@@ -23,12 +32,3 @@ derive newtype instance ixApplyIndexed :: IxApply (Hooked ps o m)
 derive newtype instance ixApplicativeIndexed :: IxApplicative (Hooked ps o m)
 derive newtype instance ixBindIndexed :: IxBind (Hooked ps o m)
 derive newtype instance ixMonadIndexed :: IxMonad (Hooked ps o m)
-
--- | A function which has access to primitive and custom hooks like UseState,
--- | UseEffect, UseRef, and UseMemo. Hook functions can be used to implement
--- | reusable, stateful logic and to implement Halogen components.
--- |
--- | Functions of this type should be constructed using the Hooks API exposed
--- | by `Halogen.Hooks`.
-type Hook ps o m (newHook :: Type -> Type) a
-  = forall hooks. Hooked ps o m hooks (newHook hooks) a
