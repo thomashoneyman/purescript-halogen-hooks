02a1fc1fe0b1b48b4d28aed0306517e8e24ad5e4
Thomas R. Honeyman <thoneyman@awakesecurity.com>
2020-08-10 20:51:58 -0700

    Update shell.nix file

--- shell.nix
+++ shell.nix
@@ -9,10 +9,10 @@ let
     repo = "easy-purescript-nix";
     rev = "7ff5a12af5750f94d0480059dba0ba6b82c6c452";
     sha256 = "0af25dqhs13ii4mx9jjkx2pww4ddbs741vb5gfc5ckxb084d69fq";
-  }) {};
+  }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation {
-  name = "setup-purescript";
+  name = "halogen-hooks";
   buildInputs = with pursPkgs; [
     pursPkgs.purs
     pursPkgs.spago
