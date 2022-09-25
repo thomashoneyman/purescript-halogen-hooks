b49d68e532b3b2b2dc85d85d2a75781951cda214
Joost ter Braak <joostterbraak@gmail.com>
2022-09-25 12:19:19 +0200

    added failing test showing Hooks being interrupted by their parent while Initializing their UseEffect state

--- test/Test/Integration/Spec.purs
+++ test/Test/Integration/Spec.purs
@@ -3,8 +3,10 @@ module Test.Integration.Spec where
 import Prelude
 
 import Test.Integration.Issue5 (rerunTickAfterInitialEffectsHook)
+import Test.Integration.Issue73 (safeInitialize)
 import Test.Spec (Spec)
 
 spec :: Spec Unit
 spec = do
   rerunTickAfterInitialEffectsHook
+  safeInitialize
